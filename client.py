274578a92534c93a03185b8b0202281c8f7f5db6
karstenes <karsten@zoobuh.com>
2015-11-13 15:28:52 -0500

    Version 1.1
    Update

--- client.py
+++ client.py
@@ -4,16 +4,21 @@ if inputt=="":
     inputt="http://google.com/"
 server=raw_input("web server (ip:port): ")
 if server=="":
-    server="127.0.0.1:1234"
+    #server="108.41.183.93:443"
+    server="localhost:443"
 server=(server[:server.index(':')], int(server[server.index(':')+1:]))
 print server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 s.connect(server) 
 s.send(inputt)
-data=s.recv(102400)
+size=int(s.recv(1024))
+ff=s.recv(10)
+print "receiving "+ff+" file with size "+str(size)
+data=s.recv(size)
+print data
 s.close()
-with open("temp.html", 'w') as temp:
+with open("temp."+ff, 'wb') as temp:
     temp.write(data)
     temp.close()
-os.system('"C:\Program Files\Internet Explorer\iexplore.exe" file://'+os.getcwd()+'/temp.html')
+os.system('"C:\Program Files\Internet Explorer\iexplore.exe" file://'+os.getcwd()+'/temp.'+ff)
 
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-import socket, urllib2
+import socket, urllib, time, os
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('127.0.0.1', 1234))
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('127.0.0.1', 443))
 s.listen(5)
 while True:
     client, address = s.accept()
@@ -8,8 +9,22 @@ while True:
     data = client.recv(1024)
     if data:
         print "received "+data
-        f = urllib2.urlopen(data)
-        content = f.read()
-        print "sending back html"
+        f = urllib.urlretrieve(data, "temp.txt")
+        csize = os.stat("temp.txt").st_size
+        print csize
+        print csize/4096
+        print csize % 4096
+        if csize % 4096 !=0:
+            csize=csize- csize % 4094
+            csize=csize+4096
+        print csize
+        content = open("temp.txt", 'rb').read(csize)
+        client.send(str(csize))
+        if data[len(data)-5:]==".html" or data[len(data)-4:]==".htm" or "." not in data[len(data)-5:] or data.endswith("/"):
+            print "sending back html"
+            client.send("html")
+        elif data[len(data)-5]=="." or data[len(data)-4]=="." or data[len(data)-3]==".":
+            print "sending back "+data[data.rfind(".")+1:]
+            client.send(data[data.rfind(".")+1:])
         client.send(content)
     client.close()
